4fa94e1dc97dfaef645bf5267b3e18d948cc42fc
jin1963 <kaojino89@gmail.com>
2025-06-13 02:38:11 +0700

    Create abi.js

---
+++ abi.js
@@ -0,0 +1,5 @@
+const stakingABI = [ 
+  {"inputs":[{"internalType":"address","name":"_g3xToken","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},
+  {"inputs":[],"name":"totalStaked","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[],"name":"rewardPool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}
+];
